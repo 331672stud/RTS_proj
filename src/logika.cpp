0077f74eaba6d94c742dda13280070472a3b14d4
331672stud <01187521@pw.edu.pl>
2026-04-05 16:55:19 +0200

    template wczytywania mapy

--- src/logika.cpp
+++ src/logika.cpp
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <osmium/handler.hpp>
+#include <osmium/io/any_input.hpp>
+#include <osmium/io/reader.hpp>
+#include <osmium/visitor.hpp>
+#include <unordered_map>
+#include <vector>
 
-int main() {
-    
+struct Point{
+    double lat;
+    double lon;
+};
+
+struct Edge{
+    long target;
+    double weight;
+};
+
+class MyHandler : public osmium::handler::Handler {
+    public:
+        void node(const osmium::Node& node) {
+        //handle node
+        }
+};
+
+void findBestPath(){
+    osmium::io::File infile("map.pbf");
+    osmium::io::Reader reader(infile);
+
+    MyHandler handler;
+    osmium::apply(reader, handler);
+
+    std::unordered_map<long, std::vector<Edge>> graph;
+
+    reader.close();
 }
